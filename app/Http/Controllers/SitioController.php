a982dcbb1133056cc459911aee2447cb1dca5ffd
afhv <andres.henaov@autonoma.edu.co>
2017-02-25 14:11:39 -0500

    Actualización de metodos del controlador

--- app/Http/Controllers/SitioController.php
+++ app/Http/Controllers/SitioController.php
@@ -6,7 +6,16 @@ use Illuminate\Http\Request;
 
 class SitioController extends Controller
 {
+    public function index() {
+        return view('Sitio/index');
+    }
 
-
+    public function verCurriculum(Request $request, $nombre) {
+        if ($nombre == 'Yaneth') {
+            return view('Sitio/Curriculums/HVYaneth');
+        } else if ($nombre == 'Andres') {
+            return view('Sitio/Curriculums/HVAndres');
+        }
+    }
 
 }
