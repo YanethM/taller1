1d617b61322a7ca5816e2e79af192b6184019158
afhv <andres.henaov@autonoma.edu.co>
2017-02-25 15:09:05 -0500

    Actualización a las vistas de hv, al index y al layout de hvs

--- resources/views/Sitio/Curriculums/HVAndres.blade.php
+++ resources/views/Sitio/Curriculums/HVAndres.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
-@section('contenido')
-<p>Hoja de vida andres</p>
+@section('nombre')
+<b>ANDRÉS FERNANDO HENAO VÉLEZ</p>
 @stop
+
+
+@section('email')
+<b>andres.henaov@autonoma.edu.co</p>
+@stop
+
+@section('nivelEducativo1')
+<p>Bachiller Académico
+</p>
+@stop
+
+@section('nivelEducativo2')
+<p>Ingeniero de Sistemas y Telecomunicaciones<br>
+    Ingeniero Electrónico
+</p>
+@stop
+
+
--- resources/views/Sitio/index.blade.php
+++ resources/views/Sitio/index.blade.php
@@ -1,1 +1,24 @@
-Hola
+<!DOCTYPE html> <html>
+<head>
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <title>Visualizador Hoja de Vida</title>
+</head>
+<body>
+    <div id="encabezado">
+        VISOR DE HOJAS DE VIDA
+    </div>
+
+    <section id="OPCION1">
+        <fieldset>
+            <a href="{!! url('TallerEstatico/verCurriculum/Yaneth') !!}">Yaneth Mejía Rendón</a>
+        </fieldset>
+    </section>
+    <section id="OPCION2">
+        <fieldset>
+            <a href="{!! url('TallerEstatico/verCurriculum/Andres') !!}">Andrés Fernando Henao Vélez</a>
+        </fieldset>
+    </section>
+
+</body>
+</html>
+
